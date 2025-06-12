31451c676f329c6db4dc47158b979a98deed9703
asrmuthu <asrmuthu57@example.com>
2025-06-12 12:51:06 +0530

    aaa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [val, setVal] = useState(0);
-
   return (
-    <div>
-      App
-      {val}
-      <button onClick={() => setVal(val + 1)} data-test-id="button">Click Me</button>
-    </div>
+    <div>App</div>
   )
 }
 
