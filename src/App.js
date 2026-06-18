ed1c07c7b611135da671c7f13b5fe9082681fea5
asrmuthu <asrmuthu57@example.com>
2026-06-18 20:27:49 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const App = () => {
-  const [count, setCount] = useState(0)
-  const handleAdd = () => {
-    setCount((pre) => pre + 1)
-  }
-  return (
-    <div><h1>{count}</h1>
-    <button onClick={handleAdd}>Add</button>
-    </div>
-  )
-}
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
 
-export default App
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/photos?_limit=20&_page=${page}`
+    );
+    const data = await res.json();
+    setItems([...items, ...data]);
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length}
+      next={fetchData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
+      {items.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
+    </InfiniteScroll>
+  );
+};
+
+export default App;
